34a7fb7d4bfe3a0bc14abfc4ace133ff723b2d70
kt.do <kt.do@samsung.com>
2015-09-02 20:41:37 +0900

    update for gorm. I did!

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,18 +2,23 @@ package controllers
 
 import (
 	"github.com/revel/revel"
-	"github.com/jinzhu/gorm"
 	"github.com/ehrudxo/revel_test1/app/models"
 )
 
 type App struct {
 	*revel.Controller
-	Tx *gorm.DB
 }
 
 func (c App) Index() revel.Result {
-	user := &models.User{Username: "keen2"}
-	// c.Tx.NewRecord(user)
-	// c.Tx.Create(user)
+	user := &models.User{Seq:0,Username: "keen2" ,Description:"keen is keen" ,Password:"1qaz" ,Mail:"ehrudxo@hotmail.com"}
+	// Db.NewRecord(user)
+	// Db.Create(&user)
 	return c.Render(user)
+}
+
+func (c App) GetUser() revel.Result {
+	user := &models.User{Seq:0,Username: "keen2" ,Description:"keen is keen" ,Password:"1qaz" ,Mail:"ehrudxo@hotmail.com"}
+	// Db.NewRecord(user)
+	// Db.Create(&user)
+	return c.RenderJson(user)
 }
--- app/models/user.go
+++ app/models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-  Seq               int64
+  Seq               int64 //`gorm:"primary_key"`
   Username          string
   Mail              string
   Password          string
